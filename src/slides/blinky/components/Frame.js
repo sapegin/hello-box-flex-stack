df11718344b2c2b360c43e6b0ea450bf5f596163
Artem Sapegin <asapegin@wayfair.com>
2019-09-20 08:32:28 +0200

    Focus escape hatck for an iframe

--- src/slides/blinky/components/Frame.js
+++ src/slides/blinky/components/Frame.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import PropTypes from 'prop-types';
 import styled, { keyframes } from 'styled-components';
 import Box from './Box';
@@ -12,10 +13,38 @@ const responsive = keyframes`
   }
 `;
 
-export const Frame = styled(Box)`
+// KINDAHACK: Show invisible bar on top of a fullscreen iframe to allow moving
+// the focus back to the presentation
+const GiveMeFocusBackBar = styled(Box)`
+	&:hover,
+	&:active {
+		cursor: grab;
+		background: ${p => p.theme.colors.primary};
+	}
+	&:active {
+		cursor: grabbing;
+	}
+`;
+
+const IFrame = styled(Box)`
 	animation: ${responsive} 20s linear infinite;
 	animation-play-state: ${p => (p.animate ? 'running' : 'paused')};
 `;
+
+export default function Frame(props) {
+	return (
+		<>
+			<GiveMeFocusBackBar
+				position="fixed"
+				top={0}
+				left={0}
+				right={0}
+				height={10}
+			/>
+			<IFrame {...props} />
+		</>
+	);
+}
 
 Frame.defaultProps = {
 	as: 'iframe',
@@ -31,5 +60,3 @@ Frame.propTypes = {
 	src: PropTypes.string.isRequired,
 	animate: PropTypes.bool,
 };
-
-export default Frame;
